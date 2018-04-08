b562de59f2320a0db8264a49353bd383a70d8b8f
Williams Chidi <williamschidi1@gmail.com>
2018-04-08 01:18:35 +0100

    Init

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require("gulp");
+var nunjucksRender = require('gulp-nunjucks-render');
+
+gulp.task('nunjucks', function() {
+  // Gets .html and .nunjucks files in pages
+  return gulp.src('parts/**/*.+(html|nunjucks)')
+  // Renders template with nunjucks
+  .pipe(nunjucksRender({
+      path: ['templates']
+    }))
+  // output files in app folder
+  .pipe(gulp.dest('outputs'))
+});
